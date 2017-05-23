963aa6ad573ec3d01ca1e039d268504d84247bd1
Yannis <Yannis@164.81.235.58>
2017-05-23 12:55:00 +0200

    extract superclass vaisseau > sprite

--- src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
@@ -1,10 +1,6 @@
 package fr.unilim.iut.spaceinvaders;
 
-public class Vaisseau {
-
-    private Position origine;
-    private Dimension dimension;
-    private int vitesse;
+public class Vaisseau extends Sprite {
 
     public Vaisseau(int longueur, int hauteur) {
         this(longueur, hauteur, 0, 0);
@@ -23,54 +19,5 @@ public class Vaisseau {
 		this.origine = positionOrigine;
 		this.vitesse = vitesse;
 	}
-
-    public boolean occupeLaPosition(int x, int y) {
-        return estAbscisseCouverte(x) && estOrdonneeCouverte(y);
-    }
-
-    private boolean estOrdonneeCouverte(int y) {
-        return (ordonneeLaPlusBasse() <= y) && (y <= ordonneeLaPlusHaute());
-    }
-
-    private boolean estAbscisseCouverte(int x) {
-        return (abscisseLaPlusAGauche() <= x) && (x <= abscisseLaPlusADroite());
-    }
-    
-    public int hauteur() {
-		return this.dimension.hauteur();
-	}
-
-	public int longueur() {
-		return this.dimension.longueur();
-	}
-
-    public int ordonneeLaPlusBasse() {
-        return this.origine.ordonnee() - this.dimension.hauteur() + 1;
-    }
-
-    public int ordonneeLaPlusHaute() {
-        return this.origine.ordonnee();
-    }
-
-    public int abscisseLaPlusADroite() {
-        return this.origine.abscisse() + this.dimension.longueur() - 1;
-    }
-
-    public int abscisseLaPlusAGauche() {
-        return this.origine.abscisse();
-    }
-
-    public void seDeplacerVersLaDroite() {
-        this.origine.changerAbscisse(this.origine.abscisse() + vitesse);
-    }
-
-    public void seDeplacerVersLaGauche() {
-        this.origine.changerAbscisse(this.origine.abscisse() - vitesse);
-    }
-
-    public void positionner(int x, int y) {
-        this.origine.changerAbscisse(x);
-        this.origine.changerOrdonnee(y);
-    }
 
 }
